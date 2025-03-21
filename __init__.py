3cc78a3af5f9b49c8801865f6ab08bd8475be316
yasais <83817157+yasais@users.noreply.github.com>
2025-03-21 09:46:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ f = Fernet(key)
 def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str test
                                                                                                                                                      
 if __name__ == "__main__":
   app.run(debug=True)
